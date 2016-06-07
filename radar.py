412ac7d80c765bc7285114ec095c4fbecfaaacf3
Julia Signell <jsignell@gmail.com>
2016-06-07 21:49:35 +0000

    using the new point-process tools

--- radar.py
+++ radar.py
@@ -46,7 +46,7 @@ class Radar:
         return self.t
      
     def from_csv(self):
-        path = self.path+'{c}/BigBOX/'.format(c=self.city)
+        path = self.path
         def dateparse(Y, m, d, H, M):
             d = pd.datetime(int(Y), int(m), int(d), int(H), int(M))
             return d
@@ -65,7 +65,7 @@ class Radar:
         return df
 
     def get_box(self, df):
-        path = self.path+'{c}/BigBOX/'.format(c=self.city)
+        path = self.path
         ll = pd.read_csv(path+self.llname, index_col=0, header=None, names=['lat','lon'])
 
         self.box = df.values.reshape(df.shape[0],140,140)
@@ -95,7 +95,7 @@ class Radar:
             for iy in range(ixy0, ixyn):
                 for it in range(it0, itn):
                     here = self.box[it+1, iy-r:iy+r+1, ix-r:ix+r+1]-self.box[it, iy, ix]
-                    if not np.isnan(np.sum(here)): #np.isnan(here).any((0,1)): 
+                    if not np.isnan(np.sum(here)): 
                         if count == 0:
                             test = here
                             count+=1
@@ -119,4 +119,85 @@ class Radar:
         ax.set_extent([self.lon[iy, ix-radius], self.lon[iy, ix+radius], self.lat[iy-radius, ix], self.lat[iy+radius, ix]])
         ax.scatter(self.lon[iy, ix], self.lat[iy, ix], edgecolor='white', facecolor='None')
         return(scat, ax, kwargs['vmax'])
+    
+    def add_buffer(self, p):
+        from geopy.distance import vincenty
 
+        edges = zip(self.lat[0, :], self.lon[0, :])
+        edges.extend(zip(self.lat[:, -1], self.lon[:, -1]))
+        edges.extend(zip(np.flipud(self.lat[-1, :]), np.flipud(self.lon[-1, :])))
+        edges.extend(zip(np.flipud(self.lat[:, 0]), np.flipud(self.lon[:, 0])))
+        
+        for it in range(p.shape[0]):
+            for ifeat in range(p.shape[1]):
+                if np.isnan(p[it, ifeat, 'centroidY']):
+                    continue
+                center = p[it, ifeat, ['centroidY', 'centroidX']].values
+                dist = min([vincenty(center, edge).kilometers for edge in edges])
+                r = (p[it, ifeat, ['area']].values/np.pi)**.5
+                if r>dist:
+                    df0 = p[it,:,:]
+                    for ichar in range(21):
+                        df0.set_value(p.major_axis[ifeat], p.minor_axis[ichar], np.nan)
+        return(p)
+
+    def get_features(self, d={}, thresh=10, min_size=20, sigma=3, const=20, return_dict=True, buffer=False):
+        '''
+        Use r package SpatialVx to identify features. 
+        
+        Parameters
+        ----------
+       
+        '''
+        from rpy2 import robjects 
+        from rpy2.robjects.packages import importr
+        from rpy2.robjects import pandas2ri
+        from pointprocess.common import import_r_tools, dotvars
+        pandas2ri.activate()
+        SpatialVx = importr('SpatialVx')
+        rsummary = robjects.r.summary
+        r_tools = import_r_tools()
+
+        ll = np.array([self.lon.flatten('F'), self.lat.flatten('F')]).T
+        for i in range(self.box.shape[0]-1):
+            hold = SpatialVx.make_SpatialVx(self.box[i,:,:], self.box[i+1,:,:], loc=ll)
+            look = r_tools.FeatureFinder_gaussian(hold, nx=self.box.shape[2], ny=self.box.shape[1], 
+                                                  thresh=thresh, smoothpar=sigma, **(dotvars(min_size=min_size)))
+            try:
+                x = rsummary(look, silent=True)[0]
+            except:
+                continue
+            px = pandas2ri.ri2py(x)
+            df0 = pd.DataFrame(px, columns=['centroidX', 'centroidY', 'area', 'OrientationAngle', 
+                                          'AspectRatio', 'Intensity0.25', 'Intensity0.9'])
+            df0['Observed'] = list(df0.index+1)
+            m = SpatialVx.centmatch(look, criteria=3, const=const)
+            p = pandas2ri.ri2py(m[12])
+            df1 = pd.DataFrame(p, columns=['Forecast', 'Observed'])
+            l = SpatialVx.FeatureMatchAnalyzer(m)
+            try:
+                p = pandas2ri.ri2py(rsummary(l, silent=True))
+            except:
+                continue
+            df2 = pd.DataFrame(p, columns=['Partial Hausdorff Distance','Mean Error Distance','Mean Square Error Distance',
+                                          'Pratts Figure of Merit','Minimum Separation Distance', 'Centroid Distance',
+                                          'Angle Difference','Area Ratio','Intersection Area','Bearing', 'Baddeleys Delta Metric',
+                                          'Hausdorff Distance'])
+            df3 = df1.join(df2)
+
+            d.update({self.time[i]: pd.merge(df0, df3, how='outer')})
+        if return_dict:
+            return(d)
+        p = pd.Panel(d)
+        if buffer:
+            return(self.add_buffer(p))
+        return(p)
+
+    def kde(self, lon, lat):
+        import scipy.stats as st
+        xx, yy = self.lon, self.lat
+        positions = np.vstack([xx.ravel(), yy.ravel()])
+        values = np.vstack([lon, lat])
+        kernel = st.gaussian_kde(values)
+        f = np.reshape(kernel(positions).T, xx.shape)
+        return(xx,yy,f)    
